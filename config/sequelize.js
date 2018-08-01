f3f01b55efde65090f088ef171e40d02329e0b4f
Dan Schultz <slifty@gmail.com>
2018-07-31 23:23:43 -0400

    Set up sequelize
    We added sequelize as a dependency already, but didn't put it to use.
    This commit contains the byproducts of sequelize init, with a slight
    modification to make sure that our env.js is loaded correctly.
    
    Note: I'm using this [guide](https://scotch.io/tutorials/getting-started-with-node-express-and-postgres-using-sequelize)
    for setting things up, since it seems reasonably close to what we're
    trying to accomplish here.
    
    Issue #26 Set up a database system

---
+++ config/sequelize.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "url": process.env.DATABASE_URL_DEV,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "url": process.env.DATABASE_URL_TEST,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "production": {
+    "url": process.env.DATABASE_URL_PROD,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  }
+}
