bedd5cf6384ecec69ae35e16835394e3601e47a0
crupar <crupar@gmail.com>
2018-09-13 22:49:38 -0700

    Add Express
    We've decided to use express because it's a fast and minimalist framework
    that works well with NodeJS and ReactJS
    
    Resolves: #29

--- index.js
+++ index.js
@@ -3,10 +3,16 @@
 */
 
 
-const logSomething = options => ({
-  options,
-  anotherOption: 'Hello!',
+import http from 'http';
+import express from 'express';
+// Express app setup
+const app = express();
+const server = http.createServer(app);
+server.listen(3000);
+server.on('listening', () => {
+  console.log('Server is listening on port: 3000');
 });
 
-const options = logSomething({ one: '1', two: '2' });
-console.log(options);
+app.get('*', (req, res) => {
+  res.end('Hello Express');
+});
