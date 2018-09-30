75c11b90eb809a86c128ac8c50abf377e259b06b
Ted Han <ted@knowtheory.net>
2018-09-30 11:05:00 -0700

    Ram in the default create-react-app instead.

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function App() {
-  return (
-    <Button variant="raised" color="primary">
-      Hello World
-    </Button>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
- 
-ReactDOM.render(<App />, document.querySelector('#app'));
+
+export default App;
