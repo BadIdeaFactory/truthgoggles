accb1681e5e21826f81e3ab7e16713ce7dddf582
Dan Schultz <slifty@gmail.com>
2019-03-08 17:20:57 -0500

    Add flag selection animation
    When a flag is selected, we want that to be visually represented with
    a change in state across ALL instances of that selected flag.  In order
    to do this without screwing everything up we needed to separate the
    background color from the flag content.
    
    This commit also removes some accidental cruft that snuck into the
    FlaggedPhrase component.
    
    Issue #52

--- src/client/components/Editor/FlaggedPhrase.js
+++ src/client/components/Editor/FlaggedPhrase.js
@@ -7,16 +7,35 @@ import { phraseType } from '../../types'
 import { calculatePhraseColor } from '../../helpers'
 
 const PhraseWrapper = styled.mark`
-  background: ${props => calculatePhraseColor(props.phrase)};
   cursor: pointer;
   color: transparent;
   margin: 0px;
   padding: 0px;
+  position: relative;
+  background: transparent;
+`
+
+const PhraseBackground = styled.div`
+  display: inline-block;
+  position: absolute;
+  background: ${props => calculatePhraseColor(props.phrase)};
+  height: 8px;
+  width: 100%;
+  left: 0px;
+  bottom: -2px;
+  transition: .25s;
+  box-sizing: content-box;
+
+  .selected & {
+    height: 100%;
+    padding: 2px;
+    bottom: -2px;
+    left: -2px;
+  }
 `
 
 class FlaggedPhrase extends React.Component {
   static propTypes = {
-    setSelectedPhrase: PropTypes.func,
     selectedPhrase: phraseType,
     phrase: phraseType,
     text: PropTypes.string
@@ -27,28 +46,26 @@ class FlaggedPhrase extends React.Component {
     setAboutToggleState(false)
   }
 
-  handleClick = () => {
-    const {
-      setSelectedPhrase,
-      phrase,
-    } = this.props
-
-    alert(phrase)
-    setSelectedPhrase(phrase)
-  }
-
   render() {
-    const { phrase, text } = this.props
+    const {
+      phrase,
+      text,
+      selectedPhrase, } = this.props
     const { dCount, rCount } = phrase
     const totalCount = phrase.dCount + phrase.rCount
     const saturation = phrase.dCount / totalCount
+    const isSelected = (selectedPhrase != null
+      && phrase.text == selectedPhrase.text)
+
     return (
       <>
         <PhraseWrapper
-          phrase={phrase}
-          onClick={this.handleClick}
+          className={isSelected?"selected":""}
         >
           { text }
+          <PhraseBackground
+            phrase={phrase}
+          />
         </PhraseWrapper>
       </>
     )
